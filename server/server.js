a21192395d7f57e3895441fc27a9589a0688740a
blueboi <blueboi@blueboi>
2022-12-05 22:02:32 +0100

    second commit

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const tracksRoutes = require("./routes/tracks");
+const usersRoutes = require("./routes/users");
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/tracks", tracksRoutes);
+app.use("/users", usersRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
